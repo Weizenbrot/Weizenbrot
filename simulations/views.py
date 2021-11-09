48ec3900ba3371e6e36aa1e4a3eb1ec478d236e9
Weizenbrot <93987326+Weizenbrot@users.noreply.github.com>
2021-11-09 19:49:19 +0100

    Delete simulations directory

--- simulations/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def boids(request):
-    return render(request, "simulations/boids.html")
-
-def gravity(request):
-    return render(request, "simulations/gravity.html")
